f8600a6ffc713d05e88211e1a61dd3ab73eaefa1
Yamangulov Andrey <yamangulov@gmail.com>
2021-01-27 22:33:22 +0300

    using props in components

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,14 +2,13 @@ import React from 'react';
 import TodoListItem from "./todo-list-item";
 
 const TodoList = () => {
-    const items = [<TodoListItem />, 'Build demo App'];
     const itemsStyle = {
         fontSize: '15px'
     };
     return (
         <ul style={itemsStyle}>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
+            <li><TodoListItem label="Drink Coffee!" /></li>
+            <li><TodoListItem label="Play preference!" important /></li>
         </ul>
     );
 };
