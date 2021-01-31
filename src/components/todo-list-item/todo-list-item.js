f2b9ac6e7405aca4b1fadde02a51dd5360a1781b
Yamangulov Andrey <yamangulov@gmail.com>
2021-01-31 19:36:03 +0300

    registration function in class as event listener

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
+    onLabelClick = () => {
+        console.log(`Done: ${this.props.label}`)
+    }
     render() {
         const {label, important = false} = this.props;
 
@@ -11,22 +14,24 @@ export default class TodoListItem extends Component {
         };
         return (
             <span className="todo-list-item">
-          <span
-              className="todo-list-item-label"
-              style={style}>
-            {label}
-          </span>
+                <span
+                    className="todo-list-item-label"
+                    style={style}
+                    onClick={this.onLabelClick}
+                >
+                    {label}
+                </span>
 
-          <button type="button"
-                  className="btn btn-outline-success btn-sm float-right">
-            <i className="fa fa-exclamation" />
-          </button>
+                <button type="button"
+                        className="btn btn-outline-success btn-sm float-right">
+                    <i className="fa fa-exclamation"/>
+                </button>
 
-          <button type="button"
-                  className="btn btn-outline-danger btn-sm float-right">
-            <i className="fa fa-trash-o" />
-          </button>
-        </span>
+                <button type="button"
+                        className="btn btn-outline-danger btn-sm float-right">
+                    <i className="fa fa-trash-o"/>
+                </button>
+            </span>
         );
     }
 }
